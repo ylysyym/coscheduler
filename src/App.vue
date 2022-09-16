588c1575952c2609cc8a843be093781caf635552
unknown <lrdwhyt@gmail.com>
2022-09-16 10:11:32 +0900

    make app layout responsive

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-    <BlockGrid />
-    <DetailPanel />
+    <BlockGrid class="block-grid" />
+    <DetailPanel class="detail-panel" />
 </template>
 
 <script setup lang="ts">
@@ -8,4 +8,13 @@ import BlockGrid from '@/blocks/BlockGrid.vue';
 import DetailPanel from '@/DetailPanel.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+.detail-panel {
+    width: 100%;
+}
+@media screen and (min-width: 960px) {
+    .detail-panel {
+        width: 300px;
+    }
+}
+</style>
