639c675190f227a4aaedfe8676a26837d08e7b14
unknown <lrdwhyt@gmail.com>
2022-10-10 15:49:19 +0900

    update creation screen date range picker to reflect value of schema type

--- src/views/CreationView.vue
+++ src/views/CreationView.vue
@@ -1,12 +1,19 @@
 <template>
     <h1>Create Schedule</h1>
     <h4>Unit of time</h4>
-    <n-select :options="schemas" />
+    <n-select
+        :options="schemas"
+        v-model:value="selectedSchema"
+        :on-update:value="resetPicker"
+    />
     <h4>Time range</h4>
     <n-date-picker
-        type="datetimerange"
+        :type="datePickerType"
         v-model:value="timeRange"
-        :time-picker-props="{ format: 'HH:mm' }"
+        :time-picker-props="timePickerProps"
+        update-value-on-close
+        :actions="null"
+        :format="pickerFormat"
     />
 </template>
 
@@ -14,6 +21,27 @@
 import { NSelect, NDatePicker } from 'naive-ui';
 import { computed, ref } from 'vue';
 import { defaultTimeUnits } from '@/models/timeUnits/defaultTimeUnits';
+
+let selectedSchema = ref(60);
+
+let timePickerProps = computed(() => {
+    let minutes = [];
+    for (let i = 0; i < 60; i += selectedSchema.value) {
+        minutes.push(i);
+    }
+    return {
+        format: 'HH:mm',
+        minutes,
+    };
+});
+
+let datePickerType = computed(() =>
+    selectedSchema.value >= 24 * 60 ? 'daterange' : 'datetimerange'
+);
+
+let pickerFormat = computed(() =>
+    selectedSchema.value >= 24 * 60 ? 'yyyy-MM-dd' : 'yyyy-MM-dd HH:mm'
+);
 
 const schemas = computed(() => {
     return defaultTimeUnits.map((unit) => {
@@ -25,4 +53,6 @@ const schemas = computed(() => {
 });
 
 let timeRange = ref();
+
+const resetPicker = () => (timeRange.value = null);
 </script>
