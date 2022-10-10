2b8e87587b20869b3e1925f11f41e425cb8608ff
unknown <lrdwhyt@gmail.com>
2022-10-11 04:11:39 +0900

    support viewing multiple availabilities in same time block

--- src/components/SquareBlock.vue
+++ src/components/SquareBlock.vue
@@ -1,11 +1,22 @@
 <template>
-    <div>
+    <div v-if="!store.isEditing">
+        <n-popover trigger="click">
+            <template #trigger>
+                <div class="block" @click="selectBlock"></div>
+            </template>
+            <div v-for="(level, i) in levels" :key="i">
+                {{ level.label }}
+            </div>
+        </n-popover>
+    </div>
+    <div v-else>
         <div class="block" @click="selectBlock"></div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue';
+import { NPopover } from 'naive-ui';
 import { useAppStore } from '@/stores/app';
 import { AvailabilityLevel } from '@/models/availability/AvailabilityLevel';
 
@@ -14,10 +25,24 @@ const props = defineProps<{
     levels: AvailabilityLevel[];
     size: number;
 }>();
-const color = computed(() => props.levels[0].color);
+
+const background = computed(() => {
+    let result = 'linear-gradient(to bottom right';
+    let n = 100 / props.levels.length;
+    let sorted = props.levels;
+    sorted.sort((a, b) => b.level - a.level);
+    for (let i = 0; i < sorted.length; i++) {
+        result += `, ${sorted[i].color} ${i * n}%, ${sorted[i].color} ${
+            (i + 1) * n
+        }%`;
+    }
+    result += ')';
+    return result;
+});
 const size = computed(() => props.size + 'px');
 
 const store = useAppStore();
+
 const selectBlock = () => {
     store.selectItem(props.id);
 };
@@ -25,7 +50,7 @@ const selectBlock = () => {
 
 <style scoped>
 .block {
-    background-color: v-bind(color);
+    background: v-bind(background);
     display: inline-block;
     height: v-bind(size);
     width: v-bind(size);
