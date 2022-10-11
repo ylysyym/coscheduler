04d375711a6eccea15ac62bd14e51873ed91f3e3
unknown <lrdwhyt@gmail.com>
2022-10-11 16:52:14 +0900

    show title and description of schedule

--- src/views/ScheduleViewer.vue
+++ src/views/ScheduleViewer.vue
@@ -1,4 +1,11 @@
 <template>
+    <div class="title-bar">
+        <n-page-header :subtitle="scheduleStore.description">
+            <template #title>
+                <strong>{{ scheduleStore.title }}</strong>
+            </template>
+        </n-page-header>
+    </div>
     <div class="control-panel">
         <ControlPanel />
     </div>
@@ -12,6 +19,7 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
+import { NPageHeader } from 'naive-ui';
 import BlockGrid from '@/components/BlockGrid.vue';
 import DetailPanel from '@/components/DetailPanel.vue';
 import ControlPanel from '@/components/ControlPanel.vue';
@@ -27,6 +35,7 @@ let detailPanelWidth = computed(() => (appStore.isEditing ? 300 : 0) + 'px');
 </script>
 
 <style scoped>
+.title-bar,
 .block-grid,
 .detail-panel,
 .control-panel {
@@ -34,17 +43,27 @@ let detailPanelWidth = computed(() => (appStore.isEditing ? 300 : 0) + 'px');
     vertical-align: top;
     width: 100%;
 }
+
+.title-bar {
+    height: 40px;
+    padding: 0;
+}
+
 @media (min-width: 900px) {
     .block-grid,
     .detail-panel,
     .control-panel {
-        height: calc(100% - 40px);
+        height: calc(100% - 80px);
     }
     .block-grid {
         width: calc(100% - 160px - v-bind(detailPanelWidth));
     }
     .detail-panel {
         width: 300px;
+    }
+
+    .title-bar {
+        padding-left: 160px;
     }
 
     .control-panel {
