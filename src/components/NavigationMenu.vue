8253370a5e52a7504b89b532bd5cd15bdaf34958
unknown <lrdwhyt@gmail.com>
2022-10-11 17:00:48 +0900

    improve menu appearance

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -1,7 +1,21 @@
 <template>
-    <strong>Menu</strong>
-    <router-link to="/">Schedule</router-link>
-    <router-link to="/create">Create</router-link>
+    <n-space size="large">
+        <div class="logo">coscheduler</div>
+        <router-link to="/">Schedule</router-link>
+        <router-link to="/create">Create</router-link>
+        <router-link to="/settings">Settings</router-link>
+    </n-space>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NSpace } from 'naive-ui';
+</script>
+
+<style scoped>
+.logo {
+    background: #672bbb;
+    color: #fff;
+    display: inline-block;
+    padding: 8px;
+}
+</style>
