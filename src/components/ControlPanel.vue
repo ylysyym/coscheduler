c8dfcb62eb1987d5f3e3214fb94862d6b26e8f58
unknown <lrdwhyt@gmail.com>
2022-11-13 04:35:16 +0900

    refactor view tab contents into separate component

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -10,59 +10,7 @@
             default-value="view"
         >
             <n-tab-pane name="view" tab="View">
-                <h3 v-if="people.length > 0">People</h3>
-                <span v-else>There's no one here!</span>
-                <n-space :vertical="!isSmallScreen">
-                    <n-checkbox-group v-model:value="uiStore.selectedNames">
-                        <n-space :vertical="!isSmallScreen">
-                            <template v-for="person in people" :key="person">
-                                <n-popover
-                                    trigger="click"
-                                    :show="showPersonPopover[person]"
-                                    @clickoutside="
-                                        showPersonPopover[person] = false
-                                    "
-                                >
-                                    <template #trigger>
-                                        <n-button
-                                            @click="
-                                                showPersonPopover[person] = true
-                                            "
-                                            tertiary
-                                            :class="{
-                                                struckout: !isChecked[person],
-                                            }"
-                                        >
-                                            {{ person }}
-                                        </n-button>
-                                    </template>
-                                    <n-space vertical>
-                                        <div class="person-header">
-                                            <strong>
-                                                {{ person }}
-                                            </strong>
-                                            &nbsp;
-                                            <n-checkbox
-                                                :value="person"
-                                                :disabled="uiStore.isEditing"
-                                            >
-                                                Show
-                                            </n-checkbox>
-                                        </div>
-                                        <PersonStats :person="person" />
-                                        <n-button
-                                            :disabled="uiStore.isEditing"
-                                            type="primary"
-                                            @click="startEditing(person)"
-                                        >
-                                            Edit
-                                        </n-button>
-                                    </n-space>
-                                </n-popover>
-                            </template>
-                        </n-space>
-                    </n-checkbox-group>
-                </n-space>
+                <ViewPanel />
             </n-tab-pane>
             <n-tab-pane
                 name="edit"
@@ -75,57 +23,23 @@
 </template>
 
 <script setup lang="ts">
-import {
-    NButton,
-    NCheckbox,
-    NCheckboxGroup,
-    NPopover,
-    NSpace,
-    NTabs,
-    NTabPane,
-    useDialog,
-} from 'naive-ui';
-import { computed, ref } from 'vue';
-import { isSmallScreen } from '@/utilities/breakpoints';
+import { NTabs, NTabPane, useDialog } from 'naive-ui';
+import { computed } from 'vue';
 import { useUiStore } from '@/stores/ui';
 import { useScheduleStore } from '@/stores/schedule';
-import PersonStats from '@/components/PersonStats.vue';
 import EditPanel from '@/components/EditPanel.vue';
+import ViewPanel from '@/components/ViewPanel.vue';
 
 const uiStore = useUiStore();
 const scheduleStore = useScheduleStore();
 
 const dialog = useDialog();
 
-let people = computed(() => scheduleStore.people);
-let showPersonPopover = ref<{ [person: string]: boolean }>({});
-
-type CheckedNameMap = { [name: string]: boolean };
-let isChecked = computed(() => {
-    return uiStore.selectedNames.reduce(
-        (result: CheckedNameMap, name: string) => {
-            result[name] = true;
-            return result;
-        },
-        {}
-    );
-});
-
 const activeTab = computed(() => (uiStore.isEditing ? 'edit' : 'view'));
 
 const isEditingExistingUser = computed(() => {
     return uiStore.isEditing && !uiStore.isJoining;
 });
-
-const startEditing = (person: string) => {
-    if (uiStore.isEditing) return;
-
-    uiStore.isEditing = true;
-    uiStore.isJoining = false;
-    uiStore.userName = person;
-    uiStore.currentEntry = scheduleStore.entries[person].slice();
-    showPersonPopover.value[person] = false;
-};
 
 const beforeChangeTab = (tabName: string) => {
     if (tabName === 'view') {
@@ -178,15 +92,6 @@ const hasChanges = computed(() => {
 <style scoped>
 .container {
     padding: 8px;
-}
-
-.struckout {
-    text-decoration: line-through;
-}
-
-.person-header {
-    display: flex;
-    justify-content: space-between;
 }
 
 .schedule-title {
