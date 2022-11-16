1c73cb204630194a2f39e72e198c371c53759f04
Lrdwhyt <Lrdwhyt@gmail.com>
2022-11-16 15:16:37 +0900

    change naive-ui primary color to violet

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,23 +1,35 @@
 <template>
-    <div class="root">
-        <NavigationMenu />
-        <div class="app-container">
-            <n-message-provider>
-                <n-dialog-provider>
-                    <RouterView />
-                </n-dialog-provider>
-            </n-message-provider>
+    <n-config-provider :theme-overrides="themeOverrides">
+        <div class="root">
+            <NavigationMenu />
+            <div class="app-container">
+                <n-message-provider>
+                    <n-dialog-provider>
+                        <RouterView />
+                    </n-dialog-provider>
+                </n-message-provider>
+            </div>
         </div>
-    </div>
+    </n-config-provider>
 </template>
 
 <script setup lang="ts">
-import { NDialogProvider, NMessageProvider } from 'naive-ui';
+import { NConfigProvider, NDialogProvider, NMessageProvider } from 'naive-ui';
+import type { GlobalThemeOverrides } from 'naive-ui';
 import NavigationMenu from '@/components/NavigationMenu.vue';
 import { useSettingsStore } from '@/stores/settings';
 
 const store = useSettingsStore();
 store.initialise();
+
+const themeOverrides: GlobalThemeOverrides = {
+    common: {
+        primaryColor: '#672bbbFF',
+        primaryColorHover: '#7f4dc6FF',
+        primaryColorSuppl: '#7f4dc6FF',
+        primaryColorPressed: '#5b26a5FF',
+    },
+};
 </script>
 
 <style scoped>
