60ebe0b13aaa27a13bc55d607f277fc9903d52d5
Lrdwhyt <Lrdwhyt@gmail.com>
2022-11-24 19:05:36 +0900

    modify interval format to indicate multi-date intervals in parentheses instead of by writing out full date

--- src/utilities/formatTimes.ts
+++ src/utilities/formatTimes.ts
@@ -1,19 +1,19 @@
-import { DateTime, Interval } from 'luxon';
+import { Interval } from 'luxon';
 import { useSettingsStore } from '@/stores/settings';
 
-const isSameDate = (a: DateTime, b: DateTime): boolean => {
-    return a.day === b.day && a.month === b.month && a.year === b.year;
+const isSameDate = (interval: Interval): boolean => {
+    return interval.count('day') <= 1;
 };
 
 export const formatInterval = (interval: Interval): string => {
     const settings = useSettingsStore();
-    const a = interval.start;
-    const b = interval.end;
-    let result = a.toFormat(settings.timestampFormat) + ' - ';
-    if (isSameDate(a, b)) {
-        result += b.toFormat(settings.timeFormat);
-    } else {
-        result += b.toFormat(settings.timestampFormat);
+    let result =
+        interval.start.toFormat(settings.timestampFormat) +
+        ' - ' +
+        interval.end.toFormat(settings.timeFormat);
+    if (!isSameDate(interval)) {
+        const dayDifference = interval.count('day') - 1;
+        result += ' (+' + dayDifference + ')';
     }
 
     return result;
